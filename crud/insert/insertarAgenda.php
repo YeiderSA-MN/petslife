54a0dcfb2857b3b0685548657296434f1152fbaf
Yeider <yeidergarzon11@gmail.com>
2023-10-17 08:59:25 -0500

    Version 2.4 - Sendmail configurado

--- crud/insert/insertarAgenda.php
+++ crud/insert/insertarAgenda.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php 
 
 // Conexion del server
@@ -62,4 +63,70 @@ function getPosts(){
       }
   
 
+=======
+<?php 
+
+// Conexion del server
+$con = mysqli_connect ('localhost', 'root', '', 'petslife');
+ if (!$con)
+ {
+	 echo 'Error de conexión';
+ }
+mysqli_select_db($con, 'petslife') or die(mysqli_error($con));
+session_start();
+	 
+//Funcion para obtener los datos en un solo array
+function getPosts(){
+
+    $posts = array();
+    $posts[0] = $_POST['servicio'];
+    $posts[1] = $_POST['local'];
+    $posts[2] = $_POST['mascota'];
+    $posts[3] = $_POST['fecha'];
+    $posts[4] = $_POST['hora'];
+    $posts[5] = $_SESSION['cedula'];
+	return $posts;
+}
+
+
+	//Guarda el array en $data
+	$data = getPosts();
+
+  //Define un query en el que se trae la informacion con la informacion del array
+	$existing_Query ="SELECT * FROM registro where id_mascota=$data[2] and fecha='$data[3]' and hora='$data[4]'";
+  //Realiza el query
+	$existing_Result = mysqli_query($con, $existing_Query);
+
+  //Si hay minimo un registro existente manda una alerta
+  if(0 < mysqli_num_rows ($existing_Result)){
+		  echo '<script type="text/javascript">
+                      alert("Ya existe una cita a esa hora para el usuario. Seleccione otra.");
+                         window.location="../../screens/agenda.php";
+                           </script>';
+	} 
+  //Si no hay hace el insert
+   else{
+    $insert_Query = "INSERT INTO registro (id_registro, id_servicio, id_local, id_mascota, fecha, hora) 
+    VALUES ('00', '$data[0]', '$data[1]', '$data[2]', '$data[3]', '$data[4]')";
+    
+
+      //realiza el query
+      $insert_Result = mysqli_query($con, $insert_Query);
+        
+      //lo ejecuta y manda las alertas
+      if ($insert_Result) {
+          echo "<script type='text/javascript'>
+                          alert('Nueva cita agregada exitosamente');
+                            window.location='../../index.php';
+                              </script>";
+        } else {
+          echo "<script type='text/javascript'>
+                          alert('Cita no insertada!');
+                            window.location='../../screens/agenda.php';
+                              </script>";
+        }
+      }
+  
+
+>>>>>>> 7c735c6 (Version 2.2 - Version web)
 ?>
--- functions/cerrarSesion.php
+++ functions/cerrarSesion.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 session_start();
@@ -12,4 +13,20 @@ session_destroy();
 header('Location: ../index.php');
 exit();
 
+=======
+<?php
+
+session_start();
+
+// Eliminar todas las variables de sesión
+session_unset();
+
+// Destruir la sesión
+session_destroy();
+
+// Redirigir al usuario a la página de inicio de sesión u otra página
+header('Location: ../index.php');
+exit();
+
+>>>>>>> 7c735c6 (Version 2.2 - Version web)
 ?>
